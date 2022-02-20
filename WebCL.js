cd9ee6c141351b1ac275a6e3a03364b9c0e8872f
Manikya Singh <manikyakpsingh@gmail.com>
2022-02-20 08:22:11 +0530

    Update WebCL.js

--- WebCL.js
+++ WebCL.js
@@ -87,7 +87,7 @@ export function GPU(){
 		let texSize = Math.ceil(Math.sqrt(size/4));
 		this.data = new Float32Array(texSize*texSize*4);
 		this.texSize = texSize;
-		let texture = null;
+		this.texture = null;
 		// this.mem = Math.pow(4, Math.ceil(Math.log(this.length) / Math.log(4)));
 		if (texSize > maxTextureSize){
 			throw new Error("ERROR: Texture size not supported!");
@@ -101,16 +101,16 @@ export function GPU(){
 			}
 		}
 		this.alloc = function(){
-			if(texture == null){
-				texture = createTexture(this.data, texSize);
+			if(this.texture == null){
+				this.texture = createTexture(this.data, texSize);
 			}
-			return texture;
+			return this.texture;
 		}
 		this.delete = function(){
-			if(texture != null){
-				gl.deleteTexture(texture);
+			if(this.texture != null){
+				gl.deleteTexture(this.texture);
 			}
-			texture = null;
+			this.texture = null;
 		}
 	}
 	function Program(inp, op, code){
